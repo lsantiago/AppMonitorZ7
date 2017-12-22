d0b446093b75f977ef2d88fcb6ccfc6476b3a39a
lsquinones <lsquinones@gmail.com>
2017-12-21 23:42:41 -0500

    Ajuste de fecha

--- src/pages/mapa/mapa.ts
+++ src/pages/mapa/mapa.ts
@@ -1,98 +1,92 @@
-  import { Component, ElementRef, ViewChild } from '@angular/core';
-  import { IonicPage, NavController, NavParams } from 'ionic-angular';
-  import { EstacionProvider } from '../../providers/estacion/estacion';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { EstacionProvider } from '../../providers/estacion/estacion';
 
-  import {Platform} from "ionic-angular";
+import { Platform } from "ionic-angular";
 
-  declare var google: any;
-  declare var MarkerClusterer: any;
+declare var google: any;
+declare var MarkerClusterer: any;
 
-  /**
-   * Generated class for the MapaPage page.
-   *
-   * See https://ionicframework.com/docs/components/#navigation for more info on
-   * Ionic pages and navigation.
-   */
+/**
+ * Generated class for the MapaPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
 
-  @IonicPage()
-  @Component({
-    selector: 'page-mapa',
-    templateUrl: 'mapa.html',
-  })
-  export class MapaPage {
-    // datos de todas las estaciones
-    private estaciones: any = [];
+@IonicPage()
+@Component({
+  selector: 'page-mapa',
+  templateUrl: 'mapa.html',
+})
+export class MapaPage {
+  // datos de todas las estaciones
+  private estaciones: any = [];
 
-    // para ubicar una estación
-    private codigoEstacion: string;
-      
-    @ViewChild('mapCanvas') mapElement: ElementRef;
-    private locations: Array<any> = [] ;
+  @ViewChild('mapCanvas') mapElement: ElementRef;
+  private locations: Array<any> = [];
 
-    constructor(private platform:Platform,
-                public estacionService: EstacionProvider,
-              private navParams: NavParams) {
-    	
-    	
-    }
+  constructor(private platform: Platform,
+    public estacionService: EstacionProvider,
+    private navParams: NavParams) {
 
-    ngOnInit(){
-      
-    }
 
-    ionViewDidLoad() {
-      this.platform.ready().then(() => {
-        this.codigoEstacion = this.navParams.get('codigo');
-        console.log('Código: ' + this.codigoEstacion);
+  }
 
-        this.estaciones = this.estacionService.listaEstaciones;
-        
-        for (let estacion of this.estaciones){
-          
-          if(estacion.latitud != null){
-              this.locations.push({name: estacion.nombre, lat: estacion.latitud, lng: estacion.longitud});  
-          }
+  ngOnInit() {
+
+  }
+
+  ionViewDidLoad() {
+    this.platform.ready().then(() => {
+      this.estaciones = this.estacionService.listaEstaciones;
+
+      for (let estacion of this.estaciones) {
+
+        if (estacion.latitud != null) {
+          this.locations.push({ name: estacion.nombre, lat: estacion.latitud, lng: estacion.longitud });
         }
+      }
 
-        
 
-        let mapEle = this.mapElement.nativeElement;
-         
 
-        let map = new google.maps.Map(mapEle, {
-              center: this.locations[0],
-              zoom: 6
-            });
+      let mapEle = this.mapElement.nativeElement;
 
-        let markers = [];
-        this.locations.forEach((markerData: any) => {
-              let infoWindow = new google.maps.InfoWindow({
-                content: `<h5>${markerData.name}</h5>`
-              });
 
-              let marker = new google.maps.Marker({
-                position: markerData,
-                map: map,
-                animation: google.maps.Animation.DROP,
-                title: markerData.name
-              });
+      let map = new google.maps.Map(mapEle, {
+        center: this.locations[0],
+        zoom: 6
+      });
 
-              markers.push(marker);
-
-              marker.addListener('click', () => {
-                infoWindow.open(map, marker);
-              });
+      let markers = [];
+      this.locations.forEach((markerData: any) => {
+        let infoWindow = new google.maps.InfoWindow({
+          content: `<h5>${markerData.name}</h5>`
         });
 
-        var markerCluster = new MarkerClusterer(map, markers, {imagePath: 'assets/imgs/cluster/m'});
-        
-        google.maps.event.addListenerOnce(map, 'idle', () => {
-              google.maps.event.trigger(map, 'resize');
-              mapEle.classList.add('show-map');
+        let marker = new google.maps.Marker({
+          position: markerData,
+          map: map,
+          animation: google.maps.Animation.DROP,
+          title: markerData.name
+        });
+
+        markers.push(marker);
+
+        marker.addListener('click', () => {
+          infoWindow.open(map, marker);
         });
       });
-    }
 
-    
-     
+      var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'assets/imgs/cluster/m' });
+
+      google.maps.event.addListenerOnce(map, 'idle', () => {
+        google.maps.event.trigger(map, 'resize');
+        mapEle.classList.add('show-map');
+      });
+    });
   }
+
+
+
+}
